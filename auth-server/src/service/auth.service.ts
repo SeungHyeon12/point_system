5a120c5b3a221bc556fb4f9975158f9a55ec51c5
Jimmy <jimmy@buttersoft.io>
2025-05-15 20:06:24 +0900

    feat: [AUTH-SERVER] userService 의 생성부분 작성

--- auth-server/src/service/auth.service.ts
+++ auth-server/src/service/auth.service.ts
@@ -1,4 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { User } from 'src/domain/user';
+import { UserRole } from 'src/domain/vo/user.role';
 import { UserRepositoryInterface } from 'src/service/interface/user.repository.interface';
 
 @Injectable()
@@ -7,4 +9,29 @@ export class AuthService {
     @Inject('USER_REPOSITORY')
     private readonly userRepository: UserRepositoryInterface,
   ) {}
+
+  async normalUserSignUp(args: { email: string; rawPassword: string }) {
+    // 비밀번호의 유효성 로직은 우선순위가 아님으로 배재
+    const newUser = await User.createUser({
+      email: args.email,
+      rawPassword: args.rawPassword,
+      userRole: UserRole.USER,
+    });
+
+    await this.userRepository.createUser(newUser);
+  }
+
+  async adminUserSignUp(args: {
+    email: string;
+    rawPassword: string;
+    role: Exclude<UserRole, 'USER'>;
+  }) {
+    const newUser = await User.createUser({
+      email: args.email,
+      rawPassword: args.rawPassword,
+      userRole: args.role,
+    });
+
+    await this.userRepository.createUser(newUser);
+  }
 }
